cef25209c62dbcc9361aeecd2b6b3d6588327fd4
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-09-11 15:41:44 +0900

    404 err fix

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -1,11 +1,22 @@
 var main = {
-    init: function () {
+    init : function () {
         var _this = this;
         $('#btn-save').on('click', function () {
+            console.log(1)
             _this.save();
         });
+
+        $('#btn-update').on('click', function () {
+            console.log(2)
+            _this.update();
+        });
+
+        $('#btn-delete').on('click', function () {
+            console.log(3)
+            _this.delete();
+        });
     },
-    save: function () {
+    save : function () {
         var data = {
             title: $('#title').val(),
             author: $('#author').val(),
@@ -16,15 +27,52 @@ var main = {
             type: 'POST',
             url: '/api/v1/posts',
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
+            contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function () {
+        }).done(function() {
             alert('글이 등록되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
+    },
+    update : function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
+
 };
 
 main.init();
